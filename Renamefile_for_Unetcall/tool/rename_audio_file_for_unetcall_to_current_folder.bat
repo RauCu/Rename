68dcbf3f82215fe09c98c88909b5c2affff3930b
RauCu <raucuqua1003@gmail.com>
2023-01-17 02:11:21 +0700

    Init
    Signed-off-by: RauCu <raucuqua1003@gmail.com>

---
+++ Renamefile_for_Unetcall/tool/rename_audio_file_for_unetcall_to_current_folder.bat
@@ -0,0 +1,40 @@
+@echo off
+color 0b
+chcp 65001
+title ĐỔI TÊN AUDIO CHO UNETCALL
+set filedrive=%~d1
+set filepath=%~p1
+set filename=%~n1
+set fileextension=%~x1
+
+if "%fileextension%" == ".mp3" set found=true
+if "%fileextension%" == ".wav" set found=true
+
+if "%found%" == "true" (
+  goto :start
+)
+
+if not defined found echo FILE KHONG DUNG & goto :info
+ECHO.
+:start
+
+for /f "tokens=1,2 delims=_" %%a in ("%filename%") do set "first=%%a" & set "second=%%b"
+set newfile=%first%_%second:~-5%_recfile%fileextension%
+set newfilepath=%filedrive%%filepath%%first%_%second:~-5%_recfile%fileextension%
+echo.
+echo Đổi tên %1
+echo.
+echo "------->       %newfile%"
+IF /I NOT %1 == "%newfilepath%" copy %1 "%newfilepath%" && echo. && echo. && echo "------->       Thành công!"
+IF /I %1 == "%newfilepath%" echo. && echo. && echo "------->       Thành công!"
+
+goto :end
+
+:info
+mode con lines=20 cols=120
+cls & echo.& echo.& echo.FILE VỪA CHỌN KHÔNG PHẢI LÀ FILE AUDIO, VUI LÒNG QUAY LẠI CHỌN FILE AUDIO (ĐUÔI FILE MP3 hoặc WAV)
+color 3f & echo. & timeout /t 15 >nul
+
+:end
+
+TIMEOUT /t 2 >nul
